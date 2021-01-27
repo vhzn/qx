7edbeb547afbe274747b48abe93a52caac3b36f2
i-chenzhe <75258500+i-chenzhe@users.noreply.github.com>
2021-01-27 16:40:59 +0800

    Update jd_dati.js

--- jd_dati.js
+++ jd_dati.js
@@ -54,7 +54,6 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
     }
-    await requireTk()
     for (let i = 0; i < cookiesArr.length; i++) {
         if (cookiesArr[i]) {
             cookie = cookiesArr[i];
