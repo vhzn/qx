feb5cc0940d99b8ffa5d5eadbda9f049209e99a6
i-chenzhe <75258500+i-chenzhe@users.noreply.github.com>
2021-01-31 00:19:16 +0800

    Update jd_dati.js
    关闭自动兑换

--- jd_dati.js
+++ jd_dati.js
@@ -92,7 +92,7 @@ async function jdImmortalAnswer() {
                 await $.wait(2 * 1000)
             }
         }
-await exchange();
+// await exchange();
         await showMsg()
     } catch (e) {
         $.logErr(e)
