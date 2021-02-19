6ee2f7f3ccc4bbd1bad6a4453185990f19ca9a50
i-chenzhe <i@2610086.com>
2021-02-19 12:52:40 +0800

    Update jd_mlyjy.js

--- jd_mlyjy.js
+++ jd_mlyjy.js
@@ -11,7 +11,7 @@
 
 更新地址：https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_mlyjy.js
 脚本仅支持Node环境，手机上的均不支持。
-cron 23 9,13,20 * *
+cron 23 9,13,20 * * *
 */
 const $ = new Env('美丽颜究院');
 const WebSocket = require("ws");
