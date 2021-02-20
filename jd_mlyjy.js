1b9088e84637f1f392376b639962db4a45954a58
i-chenzhe <i@2610086.com>
2021-02-20 23:08:18 +0800

    Update jd_mlyjy.js

--- jd_mlyjy.js
+++ jd_mlyjy.js
@@ -12,7 +12,7 @@
 
 更新地址：https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_mlyjy.js
 脚本仅支持Node环境，手机上的均不支持。
-cron 23 9,13,20 * * *
+0 0,9,13,20 * * *
 */
 const $ = new Env('美丽颜究院');
 const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
@@ -376,7 +376,7 @@ function yjy() {
             if (data.code === 200) {
               for (let vo of data.data) {
                 pname = $.product_lists.filter((x) => x.id === vo.product_id)[0].name;
-                if ((Date.now()-vo.start_at * 1000) < 2500) {
+                if ((Date.now() - vo.start_at * 1000) < 2500) {
                   console.log(`添加${vo.num}个${pname}进行生产`);
                 }
               }
@@ -409,24 +409,29 @@ function yjy() {
         ws.send(JSON.stringify(msg.stats));
         await $.wait(3000);
         ws.send(JSON.stringify(msg.shopProducts));
-        // 执行签到任务
-        await signIn();
-        //执行浏览会场任务
-        await meetingplace();
-        //执行浏览店铺任务
-        await shopView();
-        //执行浏览商品任务
-        await productView();
-        //执行每日问答
-        await answerQuestion();
-        //材料生产相关操作
-        await meterial();
-        //产品生产相关操作
-        await productProduce();
-        // 执行售卖任务
-        await sellTask();
-        //兑换福利
-        await exchange();
+        if ($.hours === 0) {
+          //兑换福利
+          await exchange();
+        } else {
+          // 执行签到任务
+          await signIn();
+          //执行浏览会场任务
+          await meetingplace();
+          //执行浏览店铺任务
+          await shopView();
+          //执行浏览商品任务
+          await productView();
+          //执行每日问答
+          await answerQuestion();
+          //材料生产相关操作
+          await meterial();
+          //产品生产相关操作
+          await productProduce();
+          // 执行售卖任务
+          await sellTask();
+          //兑换福利
+          await exchange();
+        }
       }
       await $.wait(10000);
       if (helpAuthor) {
@@ -435,7 +440,7 @@ function yjy() {
       if ($.bean > 0) {
         await showMsg();
       }
-      
+
       ws.close();
       await $.wait(2000);
       resolve();
